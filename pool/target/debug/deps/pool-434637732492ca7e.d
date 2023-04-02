6c4e2ca68448f7831ec0e275e4dcffd47906faa5
JWW127 <19317273+JWW127@users.noreply.github.com>
2023-04-02 10:41:07 -0500

    lib and basic server working with it

---
+++ pool/target/debug/deps/pool-434637732492ca7e.d
@@ -0,0 +1,5 @@
+/home/joey/Desktop/projects/rust-web-server-from-scratch/pool/target/debug/deps/pool-434637732492ca7e.rmeta: src/lib.rs
+
+/home/joey/Desktop/projects/rust-web-server-from-scratch/pool/target/debug/deps/pool-434637732492ca7e.d: src/lib.rs
+
+src/lib.rs:
---
+++ pool/target/debug/deps/pool-a181c0391c2fefa7.d
@@ -0,0 +1,5 @@
+/home/joey/Desktop/projects/rust-web-server-from-scratch/pool/target/debug/deps/pool-a181c0391c2fefa7.rmeta: src/lib.rs
+
+/home/joey/Desktop/projects/rust-web-server-from-scratch/pool/target/debug/deps/pool-a181c0391c2fefa7.d: src/lib.rs
+
+src/lib.rs:
